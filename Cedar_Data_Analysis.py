54d6ea74870a2f431a14db1aff4507cfce8b5d6f
akalia25 <32119652+akalia25@users.noreply.github.com>
2019-12-01 18:54:12 -0500

    file upload
    files upload

--- Cedar_Data_Analysis.py
+++ Cedar_Data_Analysis.py
@@ -5,3 +5,18 @@ Created on Sun Dec  1 17:58:03 2019
 
 @author: adityakalia
 """
+import pandas as pd
+
+
+def uploadfile():
+    df = pd.read_csv("/Users/adityakalia/Documents/Cedar/analytics_challenge_dataset.csv")
+    return df
+
+
+def main():
+    df = uploadfile()
+    print(df.head())
+
+
+if __name__ == '__main__':
+    main()
